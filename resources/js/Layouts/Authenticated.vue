36bf0dfcc2ee49f260809b72cec44cf901d8da90
Esi're <esiri92@gmail.com>
2022-06-21 15:55:52 +0100

    Remove unnecessary div wrapper

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -1,57 +1,55 @@
 <template>
-  <div>
-    <div class="flex h-screen flex-col bg-purple-400">
-      <nav class="flex shrink-0 justify-between bg-white px-4 py-3">
-        <a :href="route('dashboard')" class="text-2xl font-black tracking-tight">kanboard</a>
-        <div class="flex items-center">
-          <NavLink href="#">My boards</NavLink>
-          <Menu as="div" class="relative z-10 ml-3">
-            <MenuButton
-              class="rounded-full focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2">
-              <img
-                :src="$page.props.auth.user.avatarUrl"
-                :alt="$page.props.auth.user.name"
-                class="inline h-9 w-9 rounded-full" />
-            </MenuButton>
+  <div class="flex h-screen flex-col bg-purple-400">
+    <nav class="flex shrink-0 justify-between bg-white px-4 py-3">
+      <a :href="route('dashboard')" class="text-2xl font-black tracking-tight">kanboard</a>
+      <div class="flex items-center">
+        <NavLink href="#">My boards</NavLink>
+        <Menu as="div" class="relative z-10 ml-3">
+          <MenuButton
+            class="rounded-full focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2">
+            <img
+              :src="$page.props.auth.user.avatarUrl"
+              :alt="$page.props.auth.user.name"
+              class="inline h-9 w-9 rounded-full" />
+          </MenuButton>
 
-            <transition
-              enter-active-class="transition transform duration-100 ease-out"
-              enter-from-class="opacity-0 scale-90"
-              enter-to-class="opacity-100 scale-100"
-              leave-active-class="transition transform duration-100 ease-in"
-              leave-from-class="opacity-100 scale-100"
-              leave-to-class="opacity-0 scale-90">
-              <MenuItems
-                class="absolute right-0 mt-2 w-48 origin-top-right overflow-hidden rounded-md border bg-white shadow-lg focus:outline-none">
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
-                    :class="{ 'bg-gray-100': active }"
-                    class="block px-4 py-2 text-sm text-gray-700">
-                    My Profile
-                  </a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <InertiaLink
-                    :href="route('logout')"
-                    method="post"
-                    as="button"
-                    :class="{ 'bg-gray-100': active }"
-                    class="block w-full px-4 py-2 text-left text-sm text-gray-700">
-                    Log out
-                  </InertiaLink>
-                </MenuItem>
-              </MenuItems>
-            </transition>
-          </Menu>
-        </div>
-      </nav>
+          <transition
+            enter-active-class="transition transform duration-100 ease-out"
+            enter-from-class="opacity-0 scale-90"
+            enter-to-class="opacity-100 scale-100"
+            leave-active-class="transition transform duration-100 ease-in"
+            leave-from-class="opacity-100 scale-100"
+            leave-to-class="opacity-0 scale-90">
+            <MenuItems
+              class="absolute right-0 mt-2 w-48 origin-top-right overflow-hidden rounded-md border bg-white shadow-lg focus:outline-none">
+              <MenuItem v-slot="{ active }">
+                <a
+                  href="#"
+                  :class="{ 'bg-gray-100': active }"
+                  class="block px-4 py-2 text-sm text-gray-700">
+                  My Profile
+                </a>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+                <InertiaLink
+                  :href="route('logout')"
+                  method="post"
+                  as="button"
+                  :class="{ 'bg-gray-100': active }"
+                  class="block w-full px-4 py-2 text-left text-sm text-gray-700">
+                  Log out
+                </InertiaLink>
+              </MenuItem>
+            </MenuItems>
+          </transition>
+        </Menu>
+      </div>
+    </nav>
 
-      <!-- Page Content -->
-      <main class="flex-1 overflow-hidden">
-        <slot />
-      </main>
-    </div>
+    <!-- Page Content -->
+    <main class="flex-1 overflow-hidden">
+      <slot />
+    </main>
   </div>
 </template>
 
